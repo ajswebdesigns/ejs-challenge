45101c799dd6bbd5465bf04bdbd4aee426cd0d49
ajswebdesigns <andrew@ajswebdesigns.com>
2022-03-30 18:48:40 -0400

    Changed some variable names on the home route

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 // Routes
 app.get('/',(req, res)=>{
-  res.render('home', {homePageContent: homeStartingContent})
+  res.render('home', {startingContent: homeStartingContent})
 })
 
 
