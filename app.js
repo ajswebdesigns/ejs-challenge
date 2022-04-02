464dfd6187c3c60530e2cb59b8b89f8628eac2a5
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-02 15:11:23 -0400

    Challenge 18 complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { reset } = require("colors");
+const _ = require('lodash');
 const port = 3000
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -50,9 +51,9 @@ app.post('/compose',(req, res)=>{
 
 //Post Details Route
 app.get('/posts/:postName',(req,res)=>{
-  let requestedTitle = req.params.postName;
+  let requestedTitle = _.lowerCase(req.params.postName);
   posts.forEach((posts)=>{
-    let storedTitle = posts.title;
+    let storedTitle = _.lowerCase(posts.title);
     if(storedTitle === requestedTitle){
       console.log('Match has been found!')
     } else {
